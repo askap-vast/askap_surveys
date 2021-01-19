0652658b2e08090eae97c9b19ab239c5cb55750c
Andrew O'Brien <obrienan@uwm.edu>
2021-01-19 14:47:00 -0600

    added MPI

--- askap_surveys/scripts/create_vast_mocs.py
+++ askap_surveys/scripts/create_vast_mocs.py
@@ -12,7 +12,6 @@ Options:
 import argparse
 from itertools import chain
 import logging
-import multiprocessing
 from pathlib import Path
 from typing import List
 import warnings
@@ -20,6 +19,7 @@ import warnings
 from astropy.io import fits
 from astropy.wcs import WCS, FITSFixedWarning
 import mocpy
+from mpi4py import MPI
 import multiprocessing_logging
 
 
@@ -30,6 +30,10 @@ logging.basicConfig(
 )
 multiprocessing_logging.install_mp_handler()
 logger = logging.getLogger(__name__)
+
+
+def partition_list(_list, n):
+    return [_list[i::n] for i in range(n)]
 
 
 def stokes_type(stokes_str: str) -> List[str]:
@@ -48,8 +52,8 @@ def get_moc_output_dir(image_path: Path) -> Path:
     return image_path.parent.parent / output_dir_name
 
 
-def make_moc(image: Path):
-    logger.debug("Opening %s", image)
+def make_moc(image: Path, max_norder, rank):
+    logger.debug("rank: %d Opening %s", rank, image)
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", FITSFixedWarning)
         hdul = fits.open(image)
@@ -59,17 +63,22 @@ def make_moc(image: Path):
             data=hdu.data.squeeze(),
             header=WCS(hdu.header).celestial.to_header(),
         )
-        logger.info("Creating MOC for %s", image)
-        moc = mocpy.MOC.from_fits_image(hdu_squeezed, max_norder=args.max_norder)
+        logger.info("rank: %d Creating MOC for %s", rank, image)
+        moc = mocpy.MOC.from_fits_image(hdu_squeezed, max_norder=max_norder)
         output_dir = get_moc_output_dir(image)
         output_dir.mkdir(mode=0o770, exist_ok=True)
         output_path = output_dir / image.with_suffix(".moc.fits").name
         moc.write(output_path)
-        logger.info("Wrote MOC to %s", output_path)
+        logger.info("rank: %d Wrote MOC to %s", rank, output_path)
         hdul.close()
 
 
 if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    logger.info("MPI rank: %d size: %d", rank, size)
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
@@ -111,34 +120,38 @@ if __name__ == "__main__":
             "processed."
         ),
     )
-
     args = parser.parse_args()
 
-    vast_data_dir: Path = args.vast_data_dir
-    images: chain = chain()
-    logger.info("Building input image list ...")
-    for stokes in args.stokes:
-        if args.tiles:
-            logger.info("Adding Stokes %s TILE images ...", stokes)
-            images = chain(
-                images,
-                vast_data_dir.glob(
-                    f"EPOCH*/TILES/STOKES{stokes}_IMAGES/image.{stokes.lower()}.*.restored.fits"
-                ),
-            )
-        if args.combined:
-            logger.info("Adding Stokes %s COMBINED images ...", stokes)
-            images = chain(
-                images,
-                vast_data_dir.glob(
-                    f"EPOCH*/COMBINED/STOKES{stokes}_IMAGES/*.{stokes}.fits"
-                ),
-            )
-    logger.info("Finished building input image list.")
-    if args.dryrun:
-        logger.info("Would create %d MOCs.", len(list(images)))
+    if rank == 0:
+
+        vast_data_dir: Path = args.vast_data_dir
+        images: chain = chain()
+        logger.info("Building input image list ...")
+        for stokes in args.stokes:
+            if args.tiles:
+                logger.info("Adding Stokes %s TILE images ...", stokes)
+                images = chain(
+                    images,
+                    vast_data_dir.glob(
+                        f"EPOCH*/TILES/STOKES{stokes}_IMAGES/image.{stokes.lower()}.*.restored.fits"
+                    ),
+                )
+            if args.combined:
+                logger.info("Adding Stokes %s COMBINED images ...", stokes)
+                images = chain(
+                    images,
+                    vast_data_dir.glob(
+                        f"EPOCH*/COMBINED/STOKES{stokes}_IMAGES/*.{stokes}.fits"
+                    ),
+                )
+        images = list(images)
+        logger.info("Input image list contains %d images.", len(images))
+        images_split = partition_list(images, size)
     else:
-        logger.debug("Creating multiprocessing pool with %d workers.", args.nworkers)
-        pool = multiprocessing.Pool(processes=args.nworkers)
-        pool.map(make_moc, images)
-        pool.close()
+        images_split = None
+    images_split = comm.scatter(images_split, root=0)
+    logger.info("rank: %d num images: %s", rank, len(images_split))
+    for image in images_split:
+        logger.info("rank: %d Processing %s", rank, image)
+        if not args.dryrun:
+            make_moc(image, max_norder=args.max_norder, rank=rank)
