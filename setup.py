da39307c1674ce22eb6058fcfdf20ed49f8b408c
Andrew O'Brien <obrienan@uwm.edu>
2021-01-20 14:11:00 -0600

    added script to setup.py, increment version

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="askap_surveys",
-    version="0.1.0",
+    version="0.1.1",
     description="Repository of ASKAP survey footprints and other relevant surveys.",
-    author="David Kaplan",
+    author="David Kaplan, Andrew O'Brien",
     author_email="kaplan@uwm.edu",
     url="",
     packages=find_packages(),
-    # scripts=["scripts/askap_field_to_moc.py", "scripts/sky_coverage_plot.py"],
     entry_points={
         "console_scripts": [
             "sky_coverage_plot=askap_surveys.scripts.sky_coverage_plot:main",
             "askap_field_to_moc=askap_surveys.scripts.askap_field_to_moc:main",
+            "create_vast_mocs=askap_surveys.scripts.create_vast_mocs:main",
         ],
     },
     python_requires=">=3.7",
